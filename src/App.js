9dd09eabc969e36e64f0c49579167e3da98ef260
YASSD3127 <yashdevmurari97@gmail.com>
2025-12-05 12:03:07 -0600

    Updated App.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,94 @@
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+// import Navigation from './components/Navigation';
+// import Hero from './components/Hero';
+// import Portfolio from './components/Portfolio';
+// import BeforeAfter from './components/BeforeAfter';
+// import WhyChooseUs from './components/WhyChooseUs';
+// import Services from './components/Services';
+// import DesignProcess from './components/DesignProcess';
+// import { FeaturedVideos } from './components/VideoSection';
+// import Testimonials from './components/Testimonials';
+// import About from './components/About';
+// import FAQ from './components/FAQ';
+// import ContactForm from './components/ContactForm';
+// import Footer from './components/Footer';
+// import WhatsAppButton from './components/WhatsAppButton';
+// import StickyQuoteButton from './components/StickyQuoteButton';
+// import ProjectDetail from './pages/ProjectDetail';
+// import AllProjects from './pages/AllProjects';
+// import AllVideos from './pages/AllVideos';
+// import AboutDesigner from './pages/AboutDesigner';
+// import MaterialLibrary from './pages/MaterialLibrary';
+// import AllTransformations from './pages/AllTransformations';
+// import BehindTheScenes from './pages/BehindTheScenes';
+// import VideoDetail from './pages/VideoDetail';
+// import DarkModeToggle from './components/DarkModeToggle';
+
+// // Scroll to top component
+// function ScrollToTop() {
+//   const { pathname } = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+
+//   return null;
+// }
+
+// // Home Page Component
+// const HomePage = () => {
+//   return (
+//     <>
+//       {/* <DarkModeToggle /> */}
+//       <Navigation />
+//       <Hero />
+//       <Portfolio />
+//       <BeforeAfter />
+//       <WhyChooseUs />
+//       <Services />
+//       <DesignProcess />
+//       <FeaturedVideos />
+//       <Testimonials />
+//       <About />
+//       <FAQ />
+//       <ContactForm />
+//       <Footer />
+//       <WhatsAppButton />
+//       <StickyQuoteButton />
+//     </>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <Router>
+//       <ScrollToTop />
+//       <div className="min-h-screen">
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/project/:id" element={<ProjectDetail />} />
+//           <Route path="/all-projects" element={<AllProjects />} />
+//           <Route path="/all-videos" element={<AllVideos />} />
+//           <Route path="/about-designer" element={<AboutDesigner />} />
+//           <Route path="/materials" element={<MaterialLibrary />} />
+//           <Route path="/all-transformations" element={<AllTransformations />} />
+//           <Route path="/behind-the-scenes" element={<BehindTheScenes />} />
+//           <Route path="/video/:id" element={<VideoDetail />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import Portfolio from './components/Portfolio';
@@ -23,7 +112,6 @@ import MaterialLibrary from './pages/MaterialLibrary';
 import AllTransformations from './pages/AllTransformations';
 import BehindTheScenes from './pages/BehindTheScenes';
 import VideoDetail from './pages/VideoDetail';
-import DarkModeToggle from './components/DarkModeToggle';
 
 // Scroll to top component
 function ScrollToTop() {
@@ -40,7 +128,6 @@ function ScrollToTop() {
 const HomePage = () => {
   return (
     <>
-      {/* <DarkModeToggle /> */}
       <Navigation />
       <Hero />
       <Portfolio />
@@ -62,7 +149,7 @@ const HomePage = () => {
 
 function App() {
   return (
-    <Router>
+    <>
       <ScrollToTop />
       <div className="min-h-screen">
         <Routes>
@@ -77,7 +164,7 @@ function App() {
           <Route path="/video/:id" element={<VideoDetail />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// 
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
+// Fix ancestorOrigins crash
+if (!location.ancestorOrigins) {
+  location.ancestorOrigins = { contains: () => false };
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
