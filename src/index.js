325633aaa87026af67e3330c1e28d7603942a300
YASSD3127 <yashdevmurari97@gmail.com>
2025-12-05 12:06:56 -0600

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// 
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-// Fix ancestorOrigins crash
-if (!location.ancestorOrigins) {
-  location.ancestorOrigins = { contains: () => false };
+// Fix ancestorOrigins undefined safely
+if (!window.location.ancestorOrigins) {
+  window.location.ancestorOrigins = { contains: () => false };
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
